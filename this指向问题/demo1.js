211f173df3987155949597ef31e48d1203b5d3cd
jacky <874375563@qq.com>
2020-12-03 18:31:58 +0800

    我的web学习笔记第一步

---
+++ this指向问题/demo1.js
@@ -0,0 +1,30 @@
+//new的实现，修改this的指向
+
+//原型
+let proto = {
+    p1:'string'
+}
+
+//构造函数
+function OBJ(val) {
+    this.val = val
+    // return this
+}
+OBJ.prototype = proto;
+
+function _new(...params) {
+    //新建空对象，挂载构造函数原型属性
+    let obj = Object.create(OBJ.prototype);
+    console.log(obj);
+    // 绑定this指向，传入参数
+    let res = OBJ.call(obj,...params);
+    console.log(obj);
+    // 判断构造函数有无返回值
+    return typeof res === 'object' ? res : obj;
+}
+
+let obj = _new('test');
+// let obj = new OBJ('test');
+console.log(obj instanceof OBJ);//true
+console.log(obj);//{val:'test'}
+console.log(obj.__proto__);//proto
