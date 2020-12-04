189e08110d67d620cf78af68841767110d209257
jacky <jackyz9585@gmail.com>
2020-12-05 03:53:15 +0800

    添加new返回实例的原型挂载

--- js中的this/demo3.js
+++ js中的this/demo3.js
@@ -1,7 +1,16 @@
 //bind的js实现简易版
+
+/*
+    bind首先需要能够修改this的指向
+    bind返回一个函数
+    能够接受参数
+    柯里化
+ */
+
 function test(v) {
-    // this.v = v
+    this.v = v
     console.log(this.v);
+    console.log(this);
 }
 test.prototype.test = function () { console.log('test prototype'); }
 
@@ -9,34 +18,38 @@ let obj = {
     v: '1'
 }
 /**
- * 
- * @param {Object} tar 
  * @param  {Array} params 
  * @returns {function}
  */
-Function.prototype.myBind = function (tar, ...params) {
+Function.prototype.myBind = function (params) {
     //将function给self，防止混乱
+    //调用的函数
     const self = this;
-
-    if (self instanceof Function && tar instanceof Object) {
+    let outerParam = Array.from(arguments).slice(1);
+    if (self instanceof Function) {
         //返回函数 -- 函数length失真，显示的是bound的参数个数
-        return function bound() {
-            //使用instanceof判断是否为new
+        let bound = function () {
             let arg = Array.from(arguments);
-            let res = params.concat(arg);
-            return self.apply(tar, res);
+            let res = arg.concat(outerParam);
+            
+            return self.apply(params, res);
         }
+        bound.prototype = self.prototype;
+        return bound
     } else {
         throw new Error("error")
     }
 
 }
 
-let std = test.bind(obj)
-console.log(std);
-let res = test.myBind(obj);
-console.log(res);
-const t = new res(1);
-// console.log(res.length);//0 bound函数的参数个数
+// let std = test.bind(obj)
+// console.log(std);
+let res = test.myBind(obj,1,2,3);
+// res.test()
+// console.log(res);
+const t = new res(3);
+console.log(t);
+// t.test();
+console.log(res.length);//0 bound函数的参数个数
 // std();
 res();
