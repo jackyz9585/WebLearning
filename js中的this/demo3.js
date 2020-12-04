5ee98ef322e78b622c4fb727b23bf0713bb75042
jacky <jackyz9585@gmail.com>
2020-12-05 04:36:48 +0800

    将原型链继承替换为原型式继承

--- js中的this/demo3.js
+++ js中的this/demo3.js
@@ -12,16 +12,17 @@ function test(v) {
     console.log(this.v);
     console.log(this);
 }
-test.prototype.test = function () { console.log('test prototype'); }
+test.prototype.text = 'test prototype';
 
 let obj = {
     v: '1'
 }
 /**
- * @param  {Array} params 
+ * @param  {Array} context 
  * @returns {function}
  */
-Function.prototype.myBind = function (params) {
+Function.prototype.myBind = function (context) {
+
     //将function给self，防止混乱
     //调用的函数
     const self = this;
@@ -31,10 +32,20 @@ Function.prototype.myBind = function (params) {
         let bound = function () {
             let arg = Array.from(arguments);
             let res = arg.concat(outerParam);
-            
-            return self.apply(params, res);
+            //判断bound是作为构造函数还是普通函数
+            //构造函数的话this指向bound，普通函数this指向window
+            //所以是构造函数将this指向新的实例，否则指向绑定的对象context
+            // return self.apply(context, res);
+            return self.apply(this instanceof bound ? this : context, res);
         }
-        bound.prototype = self.prototype;
+        // 由于原型链继承的缺点，修改实例原型属性时会同时修改构造函数的原型属性
+        // bound.prototype = self.prototype;
+        // 所以使用一个空对象作为传输原型的一个媒介(原型式继承)
+        bound.prototype = Object.create(self.prototype);//IE9以下不支持
+
+        // let temp = function(){};
+        // temp.prototype = self.prototype;
+        // bound.prototype = new temp();
         return bound
     } else {
         throw new Error("error")
@@ -42,14 +53,18 @@ Function.prototype.myBind = function (params) {
 
 }
 
-// let std = test.bind(obj)
-// console.log(std);
-let res = test.myBind(obj,1,2,3);
-// res.test()
+let res = test.myBind(obj, 1, 2, 3);
+res();
+// console.log(res.length);//0 bound函数的参数个数
 // console.log(res);
 const t = new res(3);
-console.log(t);
-// t.test();
-console.log(res.length);//0 bound函数的参数个数
-// std();
-res();
+//原型属性测试继承
+console.log(t.text);//test prototype
+
+// 使用setPrototypeOf()不会影响构造函数的原型属性
+// Object.setPrototypeOf(t,{'text':'123'});
+// 直接操作__proto__.text会对构造函数的原型有影响
+t.__proto__.text = 'new test prototype'
+console.log(t.text);
+console.log(test.prototype.text);//new test prototype / test prototype
+
